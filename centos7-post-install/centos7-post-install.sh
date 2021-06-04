f1813b57b5e6aa577a1d5c292838eb3cf0bb8001
Mostafa Hassani <hassani0393@yahoo.com>
2021-06-04 12:25:47 +0430

    Update centos7-post-install.sh

--- centos7-post-install/centos7-post-install.sh
+++ centos7-post-install/centos7-post-install.sh
@@ -18,5 +18,15 @@ yum -y update && yum -y upgrade
 yum -y install epel-release git ansible vim dstat htop iotop glances xorg-x11-xauth atop iftop wget axel nmap bind-utils net-tools conntrack yum-utils \
 device-mapper-persistent-data links gcc nmap wget p7zip lvm2
 
+#Installing Oracle jre-8
+cd /root
+mkdir ./java
+cd /java
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies \
+--no-check-certificate 'https://docs.google.com/uc?export=download&id=16o_f7O6FTTXsfZmcA0rZGNLjSE1KfZPJ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=16o_f7O6FTTXsfZmcA0rZGNLjSE1KfZPJ" -O \
+jre-8u291-linux-x64.rpm && rm -rf /tmp/cookies.txt
+
+rpm -ivh jre-8u291-linux-x64.rpm
 
 echo "Done."
