5dab702850f07d9d961f05d3d21cf73d1fcd8fd9
Mostafa Hassani <hassani0393@yahoo.com>
2021-06-04 11:43:36 +0430

    Update centos7-post-install.sh

--- centos7-post-install/centos7-post-install.sh
+++ centos7-post-install/centos7-post-install.sh
@@ -8,15 +8,15 @@ sed -i s/^SELINUX=.*$/SELINUX=permissive/ /etc/selinux/config
 setenforce 0
 
 # Setting up firewall or turninn it off.
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
+systemctl stop firewalld
+systemctl disable firewalld
 
 # Updating and upgrading
 yum -y update && yum -y upgrade
 
 # Installing neccessary packeages.
-yum -y install git ansible vim dstat htop iotop glances xorg-x11-xauth atop iftop wget axel nmap bind-utils net-tools conntrack yum-utils \
-device-mapper-persistent-data epel-release links gcc nmap wget p7zip lvm2
+yum -y install epel-release git ansible vim dstat htop iotop glances xorg-x11-xauth atop iftop wget axel nmap bind-utils net-tools conntrack yum-utils \
+device-mapper-persistent-data links gcc nmap wget p7zip lvm2
 
 # Installing Oracle Java
 echo "Installing Java 8 (64bit)"
